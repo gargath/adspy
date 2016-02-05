786217199d1b5e27cbfb5485b82f4086f63250dc
gargath <signup@lightweaver.info>
2016-02-05 15:39:25 +0000

    Show URLs in tree

--- javascript/requests.js
+++ javascript/requests.js
@@ -1,18 +1,37 @@
+var count = 1;
+
 (function() {
 
 function listen() {
   var reloadButton = document.querySelector('.mybutton');
-  chrome.devtools.inspectedWindow.eval('console.log("Page listener called...")');
-  reloadButton.addEventListener('click', function() {tree(); chrome.devtools.inspectedWindow.eval('console.log("'+reloadButton.innerHTML+'")'); reloadButton.innerHTML ="Test"; chrome.devtools.inspectedWindow.eval('console.log("'+reloadButton.innerHTML+'")');});
+  console.log("Page listener called...");
+  reloadButton.addEventListener('click', function() {tree();});
+  
+  var treeButton = document.querySelector('.addTreeButton');
+  treeButton.addEventListener('click', function() {
+      console.log("Clear button clicked");
+      var the_tree = $('#jstree_demo').jstree(true);
+      var nodes = the_tree.get_children_dom ("1");
+      jQuery.each(nodes, function(i, val) {the_tree.delete_node(val);});
+    });
+  
+  chrome.devtools.network.onRequestFinished.addListener(
+    function(request) {
+      console.log("Request URL: "+request.request.url);
+      var the_tree = $('#jstree_demo').jstree(true);
+      count++;
+      the_tree.create_node("1",{'id':count, 'text':count+' '+request.request.url});
+      the_tree.open_node("1");
+    }
+  );
+
 }
 
 window.addEventListener('load', listen);
-chrome.devtools.inspectedWindow.eval('console.log("Listener added...")');
+console.log("Listener added...")
 })();
 
 function tree() {
-  console.log("Tree called");
-
   $('#jstree_demo').jstree({
     "core" : {
       "animation" : 0,
@@ -24,4 +43,5 @@ function tree() {
 	  }
     }
   });
+  console.log("Tree created");
 }
